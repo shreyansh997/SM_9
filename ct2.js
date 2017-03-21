0c7bbf384e8bdcf07f54a4dc230fc624696c3650
shreyansh997 <shreyansh.mehta009@gmail.com>
2017-03-21 14:55:30 +0530

    Update ct2.js

--- ct2.js
+++ ct2.js
@@ -2,11 +2,11 @@ function sayHello () {
   var name =
    document.getElementById("name").value;
   if(name == "tony")
-   var message = "<h2>marks of" + name + " are as follows! web programming=98 coa=72 mpmc=90</h2>";
+   var message = "<h2>marks of " + name + " are as follows! web programming=98 coa=72 mpmc=90</h2>";
                     
                     
   else 
-    var message= "<h2>marks of" + name +" follows web programming=00 coa=-77 mpmc=-100 </h2>";
+    var message= "<h2>marks of " + name +" follows web programming=00 coa=-77 mpmc=-100 </h2>";
 
   // document
   //   .getElementById("content")
